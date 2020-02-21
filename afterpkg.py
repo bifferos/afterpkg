c95f4fbd90a167793027e98d198ec771a8a370d2
bifferos <bifferos@gmail.com>
2020-02-21 00:54:49 +0000

    Add donothing flag (-d) default to actually running the commands

--- afterpkg.py
+++ afterpkg.py
@@ -46,7 +46,8 @@ class DependencyManager:
 
 
 class ScriptManager:
-    def __init__(self, path=Path(".")):
+    def __init__(self, path, donothing=False):
+        self.donothing = donothing
         self.before = {}
         self.after = {}
         for category in path.iterdir():
@@ -59,42 +60,54 @@ class ScriptManager:
                     self.before[package.name] = before
                 if after.exists():
                     self.after[package.name] = after
+                    
+    def run(self, script):
+        if self.donothing:
+            print(str(script))
+        else:
+            os.system(str(script))
 
     def run_before(self, package):
         if package in self.before:
-            os.system(self.before[package])
+            self.run(self.before[package])
 
     def run_after(self, package):
         if package in self.after:
-            os.system(self.after[package])
+            self.run(self.after[package])
 
 
-def build_package(dep_manager, script_manager, package_name, built):
+def build_package(dep_manager, script_manager, package_name, built, donothing):
     deps = dep_manager.lookup_deps(package_name)
     for dep in deps:
         if dep_manager.is_sbo_pkg(dep):    # Don't build anything from the core distro, assume full install
-            build_package(dep_manager, script_manager, dep, built)
+            build_package(dep_manager, script_manager, dep, built, donothing)
     if package_name in built:
         return
     script_manager.run_before(package_name)
-    print("running sbopkg -B -i %s" % package_name)
+    command = "sbopkg -B -i %s" % package_name
+    if donothing:
+        print(command)
+    else:
+        os.system(command)
     script_manager.run_after(package_name)
     built.add(package_name)
 
 
 def build_packages(args):
     dep = DependencyManager()
-    scripts = ScriptManager()
+    scripts = ScriptManager(Path("."), args.donothing)
     built_packages = set()
     if args.packages:
         for pkg_name in args.packages:
-            build_package(dep, scripts, pkg_name, built_packages)
+            build_package(dep, scripts, pkg_name, built_packages, args.donothing)
     else:
         print()
 
 
 def main():
     parser = argparse.ArgumentParser(prog='afterpkg')
+    parser.add_argument("-d", "--donothing",  default=False, action="store_true",
+                        help="Don't actually build any packages, just list the commands that would be run")
     parser.add_argument("packages", default=False, nargs="+",
                         help="Package(s) to build")
     args = parser.parse_args()
